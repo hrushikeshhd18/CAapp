9314a536506c5ab1b511d853cb330a6e0dc5afc2
hrushikeshdesai <hrutikdesai1@gmail.com>
2022-01-14 19:47:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/caapp/HomeActivity.java
@@ -0,0 +1,138 @@
+package com.example.caapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    Button b1,b2,b3,b4,b5,b6,b7;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button b1=findViewById(R.id.button);
+        Button b2=findViewById(R.id.button2);
+        Button b3=findViewById(R.id.button3);
+        Button b4=findViewById(R.id.button4);
+        Button b5=findViewById(R.id.button5);
+        Button b6=findViewById(R.id.button6);
+        Button b7=findViewById(R.id.button7);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                String[] recipients={"gokul@vritikatechnologies.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+                intent.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+                intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+                intent.setType("text/html");
+                intent.setPackage("com.google.android.gm");
+                startActivity(Intent.createChooser(intent, "Send mail"));
+
+            }
+        });
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                String[] recipients={"gokul@vritikatechnologies.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+                intent.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+                intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+                intent.setType("text/html");
+                intent.setPackage("com.google.android.gm");
+                startActivity(Intent.createChooser(intent, "Send mail"));
+
+            }
+        });
+
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                String[] recipients={"gokul@vritikatechnologies.com.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+                intent.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+                intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+                intent.setType("text/html");
+                intent.setPackage("com.google.android.gm");
+                startActivity(Intent.createChooser(intent, "Send mail"));
+
+            }
+        });
+
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                String[] recipients={"gokul@vritikatechnologies.com.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+                intent.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+                intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+                intent.setType("text/html");
+                intent.setPackage("com.google.android.gm");
+                startActivity(Intent.createChooser(intent, "Send mail"));
+
+            }
+        });
+
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                String[] recipients={"gokul@vritikatechnologies.com.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+                intent.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+                intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+                intent.setType("text/html");
+                intent.setPackage("com.google.android.gm");
+                startActivity(Intent.createChooser(intent, "Send mail"));
+
+            }
+        });
+
+
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                String[] recipients={"gokul@vritikatechnologies.com.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+                intent.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+                intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+                intent.setType("text/html");
+                intent.setPackage("com.google.android.gm");
+                startActivity(Intent.createChooser(intent, "Send mail"));
+
+            }
+        });
+
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HomeActivity.this,FormActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
